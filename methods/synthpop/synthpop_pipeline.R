0bef753f3695b301d29a8e6bfd39707209ebfeb1
Greg Mingas <gmingas@turing.ac.uk>
2020-01-02 17:03:20 +0000

    Add first unit test - put utility functions in separate file

--- methods/synthpop/synthpop_pipeline.R
+++ methods/synthpop/synthpop_pipeline.R
@@ -39,23 +39,7 @@ library(synthpop)
 
 
 ### Utility functions ###
-read_data <- function(dataset_name){
-  
-  if (dataset_name == "Polish") {
-    # Polish quality of life survey (2011)
-    data_full <- SD2011
-    levels(data_full$edu) <- c("NONE", "VOC", "SEC", "HIGH")
-    
-  } else if (dataset_name == "CensusUK2011"){
-    # ONS Census 2011 teaching file
-    data_full <- read.csv("../../datasets/rft-teaching-file/2011 Census Microdata Teaching File.csv",
-                          header=TRUE, skip=1)
-    
-  } else stop("Unknown datsaset")
-  
-  return(data_full)
-  
-}
+source("util.R")
 
 
 
---
+++ methods/synthpop/tests/test_synthpop_pipeline.R
@@ -0,0 +1,19 @@
+### Unit tests for synthpop pipeline ###
+
+# source the utility functions file
+setwd("../")
+source("util.R")
+
+# install packages
+list.of.packages <- c("testthat") # dplyr for covariance
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
+
+# load testthat
+library("testthat")
+
+# test the data input function
+test_that("Dimensions of inputted dfs are as expected", {
+  expect_equal(dim(read_data("Polish")), c(5000, 35))
+  expect_equal(dim(read_data("CensusUK2011")), c(569741, 18))
+})
