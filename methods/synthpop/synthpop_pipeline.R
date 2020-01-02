69a0184ee68bdddba17f581cc7bb7e1b676bd34b
Greg Mingas <gmingas@turing.ac.uk>
2020-01-02 14:07:43 +0000

    Specify CRAN mirror argument when installing

--- methods/synthpop/synthpop_pipeline.R
+++ methods/synthpop/synthpop_pipeline.R
@@ -31,7 +31,7 @@
 ### Import libraries ###
 list.of.packages <- c("synthpop", "dplyr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+if(length(new.packages)) install.packages(new.packages, repos = "http://cran.us.r-project.org")
 
 library(synthpop)
 library(dplyr)
