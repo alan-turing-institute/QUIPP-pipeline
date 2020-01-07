b6c782b2d79f6543ecd0933a3bc0dc72b0004f9e
crangelsmith <camila.rangel.smith@gmail.com>
2020-01-07 16:16:47 +0000

    adding actual test on simmulated annealing analysis

--- methods/simulated-annealing/test_MicrosimulationOptimiser.py
+++ methods/simulated-annealing/test_MicrosimulationOptimiser.py
@@ -6,9 +6,9 @@ from MicrosimulationOptimiser import MicrosimulationOptimiser
 
 def test_MicrosimulationOptimiser():
 
-    age = pd.read_csv(os.path.join(os.getcwd(), "..", "..", "datasets", "SimpleWorld", "age.csv"))
-    sex = pd.read_csv(os.path.join(os.getcwd(), "..", "..", "datasets", "SimpleWorld", "sex.csv"))
-    ind = pd.read_csv(os.path.join(os.getcwd(), "..", "..", "datasets", "SimpleWorld", "ind-full.csv"))
+    age = pd.read_csv(os.path.join(os.getcwd(), "datasets", "SimpleWorld", "age.csv"))
+    sex = pd.read_csv(os.path.join(os.getcwd(), "datasets", "SimpleWorld", "sex.csv"))
+    ind = pd.read_csv(os.path.join(os.getcwd(), "datasets", "SimpleWorld", "ind-full.csv"))
 
     age_conditions = [ind["age"] < 50, ind["age"] >= 50]
     sex_conditions = [ind["sex"] == "m", ind["sex"] == "f"]
@@ -17,13 +17,11 @@ def test_MicrosimulationOptimiser():
     ind_array = np.array([np.select(age_conditions, range(len(age_conditions)), default=None),
                           np.select(sex_conditions, range(len(sex_conditions)), default=None)]).transpose()
 
-    print("Seed population:\n {}".format(ind_array))
 
     np.random.seed(1)
     region = 0
 
     opt = MicrosimulationOptimiser(ind_array, age.to_numpy()[region], sex.to_numpy()[region])
-    print("Initial state:", opt.state)
 
     opt.Tmax = 100
     opt.Tmin = 0.1
@@ -31,19 +29,11 @@ def test_MicrosimulationOptimiser():
 
     synthetic_population = ind.iloc[population_ids]
 
-    print("Region {}".format(region))
 
-    print("\nAge")
-    print("Under 50: expected {}, got {}".format(age["a0.49"].iloc[region],
-                                                 len(synthetic_population[synthetic_population["age"] < 50])))
-    print("50 and over: expected {}, got {}".format(age["a.50+"].iloc[region],
-                                                    len(synthetic_population[synthetic_population["age"] >= 50])))
+    assert age["a0.49"].iloc[region] ==len(synthetic_population[synthetic_population["age"] < 50])
 
-    print("\nSex")
-    print("Males: expected {}, got {}".format(sex["m"].iloc[region],
-                                              len(synthetic_population[synthetic_population["sex"] == "m"])))
-    print("Females: expected {}, got {}".format(sex["f"].iloc[region],
-                                                len(synthetic_population[synthetic_population["sex"] == "f"])))
+    assert age["a.50+"].iloc[region]== len(synthetic_population[synthetic_population["age"] >= 50])
 
+    assert sex["m"].iloc[region]==len(synthetic_population[synthetic_population["sex"] == "m"])
 
-    assert False
+    assert sex["f"].iloc[region]==len(synthetic_population[synthetic_population["sex"] == "f"])
