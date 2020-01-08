86c4f89e3834938133cd4af614a69716d6fc5cf0
Louise Bowler <LouiseABowler@users.noreply.github.com>
2020-01-08 14:47:20 +0000

    Parametrise test as an example

--- methods/simulated-annealing/test_MicrosimulationOptimiser.py
+++ methods/simulated-annealing/test_MicrosimulationOptimiser.py
@@ -19,28 +19,38 @@ def simpleworld_example():
     return ind, age, sex, age_conditions, sex_conditions
 
 
-def test_MicrosimulationOptimiser(simpleworld_example):
+# Hardcode region and counts of <50 years, >=50 years, male and female.
+# In this simple problem, the optimiser should be able to exactly match these hardcoded expected values.
+region_constraints = [(0, 8, 4, 6, 6),
+                      (1, 2, 8, 4, 6),
+                      (2, 7, 4, 3, 8)]
+
+
+@pytest.mark.parametrize("region,young,old,male,female", region_constraints)
+def test_MicrosimulationOptimiser(region, young, old, male, female, simpleworld_example):
     """Run the simulated annealing algorithm on Region 0 in the SimpleWorld example and check that we get the correct
     number of people in each of the categories."""
 
+    np.random.seed(1)
     ind, age, sex, age_conditions, sex_conditions = simpleworld_example
 
-    region = 0
-    np.random.seed(1)
-
-    # Now create an array that holds the individuals and how their properties correspond to the constraints
+    # Give properties of below/above 50 and male/female to each individual to match constraints
     ind_array = np.array([np.select(age_conditions, range(len(age_conditions)), default=None),
                           np.select(sex_conditions, range(len(sex_conditions)), default=None)]).transpose()
 
+    # Initialise the optimisation class with the array of individuals and arrays of age and sex constraints (counts)
     opt = MicrosimulationOptimiser(ind_array, age.to_numpy()[region], sex.to_numpy()[region])
 
+    # We'll use the same temperature settings as in the example shown in the notebook
     opt.Tmax = 100
     opt.Tmin = 0.1
-    population_ids, error = opt.anneal()
 
+    # Annealing process swaps individuals until constraints are met, then we extract the IDs of the synthetic population
+    population_ids, error = opt.anneal()
     synthetic_population = ind.iloc[population_ids]
 
-    assert age["a0.49"].iloc[region] == len(synthetic_population[synthetic_population["age"] < 50])
-    assert age["a.50+"].iloc[region] == len(synthetic_population[synthetic_population["age"] >= 50])
-    assert sex["m"].iloc[region] == len(synthetic_population[synthetic_population["sex"] == "m"])
-    assert sex["f"].iloc[region] == len(synthetic_population[synthetic_population["sex"] == "f"])
+    # Compare numbers of people with each characteristic in the synthetic population with the expected values
+    assert len(synthetic_population[synthetic_population["age"] < 50]) == young
+    assert len(synthetic_population[synthetic_population["age"] >= 50]) == old
+    assert len(synthetic_population[synthetic_population["sex"] == "m"]) == male
+    assert len(synthetic_population[synthetic_population["sex"] == "f"]) == female
