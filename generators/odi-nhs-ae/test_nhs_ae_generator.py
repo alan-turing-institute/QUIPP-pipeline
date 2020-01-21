8c44e6082557b51618106f590b161c4ab8aceace
crangelsmith <camila.rangel.smith@gmail.com>
2020-01-21 14:59:39 +0000

    CI tests for generator odi-nhs-ae

--- generators/odi-nhs-ae/test_nhs_ae_generator.py
+++ generators/odi-nhs-ae/test_nhs_ae_generator.py
@@ -5,6 +5,9 @@ import os
 
 def test_generator():
 
+
+    os.system('curl -o "./data/London postcodes.csv" https://www.doogal.co.uk/UKPostcodesCSV.ashx?region=E12000007')
+
     main(20, 'test_generated.csv', 23414)
 
     with open(os.path.join(os.getcwd(), "..", "..", "datasets", "generated", "odi_nhs_ae", "test_reference.csv")) as r,\
