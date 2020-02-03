1673fc7e7e99c1fee33180c5b1dbe049645f5408
Kasra Hosseini <khosseini@IMAC-ATI0169.corp.turing.ac.uk>
2020-02-03 13:00:04 +0000

    Change the parameters file Review ctgab_main and base.py

--- synth-methods/Base/base.py
+++ synth-methods/Base/base.py
@@ -6,57 +6,54 @@ import os
 import pandas as pd
 import re
 import sys
-import ntpath
 
 # --- Base class to be used in synth-methods 
 class SynthesizerBase:
 
     def __init__(self):
         self.dataset_name = None
-        self.synthesis_name = None
         self.input_data = None
         self.metadata = None
         self.parameters = None
         self.model = None
         self.synthetic_data = None
 
-    def read_data(self, csv_path, json_path, synthesis_name="synth_default", store_internally=False, verbose=True):
+    def read_data(self, csv_path, json_path, store_internally=True, verbose=True):
         """Reads input data from .csv file and metadata from .json file.
         Stores the data and metadata within the class object if
-        store_internally=True (default False). User has to define a synthesis name (synth_name)
+        store_internally=True (default). User can define a synthesis name (synth_name)
         Returns data (pandas dataframe) and metadata (dictionary)"""
 
+        # --- CHECK csv and json files
         if not os.path.isfile(csv_path):
             sys.exit("File does not exist: %s" % csv_path)
-
         if not os.path.isfile(json_path):
             sys.exit("File does not exist: %s" % json_path)
 
-        # Extract csv filename only, remove extension and use as dataset_name
-        # synthesis_name is provided by user
-        self.dataset_name = re.split(".csv", os.path.basename(csv_path), flags=re.IGNORECASE)[0]
-        self.synthesis_name = synthesis_name
-
         # Read csv from file
         data = pd.read_csv(csv_path)
+        # Extract csv filename only, remove extension and use as dataset_name
+        self.dataset_name = re.split(".csv", os.path.basename(csv_path), flags=re.IGNORECASE)[0]
 
         # Read json from file
         with open(json_path) as metadata_json:
             metadata = json.load(metadata_json)
 
+        # >>> CHECK JSON file
         if not ('columns' in metadata):
             sys.exit("\nMetadata file does not contain 'columns' key. Please refer to the documentation.")
 
         metadata_cols_number = len(metadata['columns'])
         if not metadata_cols_number == data.shape[1]:
             sys.exit("\nNumber of columns mismatch between data ({}) and metadata ({})".format(metadata_cols_number,
-                                                                                             data.shape[1]))
+                                                                                               data.shape[1]))
 
         for index, name_data in enumerate(data.columns):
             name_metadata = metadata['columns'][index]['name']
             if not (name_data == name_metadata):
                 print("\n[WARNING]: Column name mismatch between data and metadata: {} {}".format(name_data,
                                                                                                   name_metadata))
+        # <<<
 
         # Store data and metadata internally
         if store_internally:
@@ -64,8 +61,8 @@ class SynthesizerBase:
             self.metadata = metadata
             if verbose:
                 print("\n[INFO] Stored input data and metadata internally")
-
-        return data, metadata
+        else:
+            return data, metadata
 
     def fit_synthesizer(self):
         """Leave empty. Method-specific implementation should be provided in child class of Base"""
--- synth-methods/CTGAN/ctgan_main.py
+++ synth-methods/CTGAN/ctgan_main.py
@@ -17,7 +17,7 @@ from base import SynthesizerBase
 class SynthesizerCTGAN(SynthesizerBase):
 
     def __init__(self):
-        self.num_samples_for_fitting = None
+        self.num_samples_to_fit = None
         self.num_samples_to_synthesise = None
         self.discrete_column_names = None
         self.num_epochs = None
@@ -25,77 +25,72 @@ class SynthesizerCTGAN(SynthesizerBase):
         # instantiate SynthesizerBase from Base directory
         super().__init__()
 
-    def read_data(self, csv_path, json_path, synthesis_name, store_internally=False, verbose=True):
+    def read_data(self, csv_path, json_path, store_internally=True, verbose=True):
         """This is intended as a place to call the parent read_data(...) and also add method-specific
         data pre-processing. If no pre-processing is required then it can just
         call the parent read_data(...)"""
-        return super().read_data(csv_path, json_path, synthesis_name,
-                                 store_internally, verbose)
+        return super().read_data(csv_path, json_path, store_internally, verbose)
 
-    def fit_synthesizer(self, csv_path, metadata_json_path, synthesis_name, parameters_json_path,
-                        store_internally=False, use_stored_inputs=False, verbose=True):
-        """Reads input data and metadata, fits CTGAN model and stores it internally.
+    def fit_synthesizer(self, parameters_json_path, csv_path=False, metadata_json_path=False, 
+                        store_internally=False, use_stored_inputs=True, 
+                        verbose=True):
+        """Fits CTGAN model and stores it internally
+        Uses pre-stored data and metadata if use_stored_inputs=True (default).
         Stores the data and metadata within the class object if store_internally=True (default False).
-        Uses pre-stored data and metadata if use_stored_inputs=True (default False).
         Returns the fitted model."""
 
         # Read data and metadata
         if use_stored_inputs:
             if self.input_data is None:
                 sys.exit("\nNo input data file is stored in the object.")
-            elif self.metadata is None:
+            if self.metadata is None:
                 sys.exit("\nNo metadata file is stored in the object.")
-            else:
-                if verbose:
-                    print("\n[INFO] Reading data and metadata previously stored within class object")
-                input_data = self.input_data
-                metadata = self.metadata
+            if verbose:
+                print("\n[INFO] Reading data and metadata previously stored within class object")
         else:
             if verbose:
                 print("\n[INFO] Reading input data and metadata from disk")
-            input_data, metadata = self.read_data(csv_path, metadata_json_path,
-                                                  synthesis_name, store_internally, verbose)
+            self.input_data, self.metadata = self.read_data(csv_path, metadata_json_path, store_internally, verbose)
 
         if verbose:
             print("\nSUMMARY")
             print("-------")
-            print("Dataframe dimensions\n#rows:    {}\n#columns: {}".format(input_data.shape[0],
-                                                                              input_data.shape[1]))
+            print("Dataframe dimensions\n#rows:    {}\n#columns: {}".format(self.input_data.shape[0],
+                                                                            self.input_data.shape[1]))
             print("\nColumn name \t Type\n" + 11 * "--")
-            for col in metadata['columns']:
+            for col in self.metadata['columns']:
                 print("{} \t {}".format(col['name'], col['type']))
 
         # Extract parameters from json parameter file
         with open(parameters_json_path) as parameters_json:
             self.parameters = json.load(parameters_json)
-        self.num_samples_for_fitting = self.parameters['parameters'][0]['value']
-        self.num_epochs = self.parameters['parameters'][1]['value']
-        self.random_state = self.parameters['parameters'][3]['value']
+        self.num_samples_to_fit = self.parameters['parameters']['num_samples_to_fit']
+        self.num_epochs = self.parameters['parameters']['num_epochs']
+        self.random_state = self.parameters['parameters']['random_state']
         if verbose:
             print(f"\n[INFO] Reading CTGAN parameters from json file:\n"
-                  f"num_samples_for_fitting = {self.num_samples_for_fitting}\n"
+                  f"num_samples_to_fit = {self.num_samples_to_fit}\n"
                   f"num_epochs = {self.num_epochs}\n"
                   f"random_state = {self.random_state}\n")
 
         # Extract discrete column names list from metadata
-        # NOTE: The list of discrete types needs to be updated when the format of the metadata is finalised
-        self.discrete_column_names = [col['name'] for col in metadata['columns']
+        # XXX NOTE: The list of discrete types needs to be updated when the format of the metadata is finalised
+        self.discrete_column_names = [col['name'] for col in self.metadata['columns']
                                       if col['type'] in ['categorical', 'ordinal', 'integer']]
 
         # Draw random sample from input data with requested size
-        if self.num_samples_for_fitting == 0:
+        if self.num_samples_to_fit == 0:
             sys.exit('\nNumber of samples for fitting cannot be 0')
-        elif self.num_samples_for_fitting != -1:
+        elif self.num_samples_to_fit != -1:
             if verbose:
-                print(f"\n[INFO] Sampling {self.num_samples_for_fitting} rows from input data")
-            data_sample = input_data.sample(n=self.num_samples_for_fitting,
-                                            random_state=self.random_state)
+                print(f"\n[INFO] Sampling {self.num_samples_to_fit} rows from input data")
+            self.data_sample = self.input_data.sample(n=self.num_samples_to_fit, random_state=self.random_state)
         else:
-            data_sample = input_data
+            self.data_sample = self.input_data
 
         if verbose:
             print("[INFO] Summary of the data frame that will be used for fitting:")
-            print(data_sample.describe())
+            print(self.data_sample.describe())
 
         # Instantiate CTGANSynthesizer object
         ctgan = CTGANSynthesizer()
@@ -103,49 +98,48 @@ class SynthesizerCTGAN(SynthesizerBase):
         # Fit the model
         if verbose:
             print(f"\n[INFO] Fitting the CTGAN model, total number of epochs: {self.num_epochs}")
-        ctgan.fit(data_sample, self.discrete_column_names, epochs=self.num_epochs)
+        ctgan.fit(self.data_sample, self.discrete_column_names, epochs=self.num_epochs)
 
         # Store the model
         self.model = ctgan
 
-        return ctgan
+    def synthesize(self, num_samples_to_synthesize=False, store_internally=True,
+                   output_path=False, verbose=True):
+        """Create synthetic data set using the fitted model. Stores the synthetic data
+        within the class object if store_internally=True (default) and outputs the
+        synthetic data to disk if output_filename is provided (default False)."""
 
-    def synthesize(self, num_samples_to_synthesize=False, store_internally=False,
-                   output_filename=False, verbose=True):
-        """Creates synthetic data set using the fitted model. Stores the synthetic data
-        within the class object if store_internally=True (default False) and outputs the
-        synthetic data to disk if output_filename is provided (default False).
-        Returns the synthetic data frame."""
-
+        self.num_samples_to_synthesize = num_samples_to_synthesize
         # Synthesize the samples
-        if num_samples_to_synthesize:
-            synthetic_data = self.model.sample(num_samples_to_synthesize)
-        else:
-            synthetic_data = self.model.sample(self.parameters['parameters'][2]['value'])
+        if not self.num_samples_to_synthesize:
+            print(f"[INFO] number of samples to synthesize is set to {len(self.data_sample)}")
+            self.num_samples_to_synthesize = len(self.data_sample)
+        synthetic_data = self.model.sample(self.num_samples_to_synthesize)
+
         if verbose:
             print(f"\n[INFO] Synthesis: Created synthetic data set with the following characteristics:\n"
                   f"#rows:    {synthetic_data.shape[0]}\n#columns: {synthetic_data.shape[1]}")
             print(f"Column names:  {[col for col in synthetic_data.columns]}\n")
+
+        # Write data to disk if needed
+        if output_path:
+            output_path_pardir = os.path.join(output_path, os.path.pardir)
+            import ipdb; ipdb.set_trace()
+            if not os.path.isdir(output_path_pardir):
+                os.makedirs(output_path_pardir)
+            if os.path.isfile(output_path):
+                print(f"[WARNING] Output file {output_path} already exists and will be overwritten")
+            synthetic_data.to_csv(output_path)
+            if verbose:
+                print(f"[INFO] Stored synthesized dataset to file: {output_path}")
 
         # Store internally if needed
         if store_internally:
             self.synthetic_data = synthetic_data
             if verbose:
                 print("[INFO] Stored synthesised dataset internally")
-
-        # Write data to disk if needed
-        if output_filename:
-            output_path = f'../../synthetic-output/{self.synthesis_name}/{self.dataset_name}/CTGAN/'
-            output_filepath = output_path + output_filename
-            if not os.path.isdir(output_path):
-                os.makedirs(output_path)
-            if os.path.isfile(output_filepath):
-                print(f"[WARNING] Output file {output_filepath} already exists and will be overwritten")
-            synthetic_data.to_csv(output_filepath)
-            if verbose:
-                print(f"[INFO] Stored synthesized dataset to file: {output_filepath}")
-
-        return synthetic_data
+        else:
+            return synthetic_data
 
 if __name__ == "__main__":
     # Test if it works
@@ -153,18 +147,10 @@ if __name__ == "__main__":
 
     path2csv = os.path.join("tests", "data", "test_CTGAN_io.csv")
     path2meta = os.path.join("tests", "data", "test_CTGAN_io_data.json") 
-    # XXX Do we need this?
-    synthesis_name = "Synthesis00"
     path2params = os.path.join("tests", "parameters", "ctgan_parameters.json")
-    syn.fit_synthesizer(path2csv, path2meta,
-                        synthesis_name, path2params, 
-                        store_internally=False, 
-                        use_stored_inputs=False, 
-                        verbose=True)
-    output = syn.synthesize(num_samples_to_synthesize=150, 
-                            store_internally=True, 
-                            output_filename="test.csv", 
-                            verbose=True)
+    syn.read_data(path2csv, path2meta)
+    syn.fit_synthesizer(path2params)
+    output = syn.synthesize(num_samples_to_synthesize=200, output_path="./test.csv")
     print("Output df head:\n", output.head())
     # print(syn.model)
     # print(syn.parameters)
