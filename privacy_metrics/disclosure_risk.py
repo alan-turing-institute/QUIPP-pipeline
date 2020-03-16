da39ba0ca72d6596c3be52e97bce00a25639208f
kasra-hosseini <khosseini@turing.ac.uk>
2020-03-16 17:25:11 +0000

    Add three output modes: 1 (only max), 2 (values >= max * threshold) and 3 (full probability)

--- privacy_metrics/disclosure_risk.py
+++ privacy_metrics/disclosure_risk.py
@@ -14,19 +14,20 @@ path_released_ds = "../synth-output/synthpop-example-2/synthetic_data_*.csv"
 path_intruder_ds = "../synth-output/synthpop-example-2/intruder_data.csv"
 # save maximum values/indices for all intruder's rows in a dictionary
 path_save_max_values = "./dict_max_matches.pkl"
-# if full_probability = True, save the pdfs (see below for more info)
+# if output_mode = 3, save the pdfs (see below for more info)
 path_save_p_dist_all = "./p_dist_all.pkl"
 # XXX REMOVE? 
 # column that contains indices of the original (not synthesized) dataset
 # when matching rows between two datasets, ignore indx_column 
 indx_column = "idx"
-# if True: 
+# mode 1: only return the maximum
+#    this is mode is idential to mode 2 with threshold_max = 0.999
+# mode 2: return all values more than np.max(p.d.f of one intruder row)*threshold_max
+# mode 3: return full probability distribution for each intruder row 
 #    output probability distribution over each row of the released data, 
 #    e.g., if the released data has 9000 rows and the intruder's data has 1000 rows, 
 #    a numpy array with 1000 x 9000 dimensions will be created.
-# if False:
-#    only max values and their indices will be returned
-full_probability = True
+output_mode = 1
 # the following value will be used to extract "found" rows from the released data:
 # np.max(p.d.f of one intruder row)*threshold_max
 threshold_max = 0.8
@@ -52,6 +53,11 @@ df_itdr["idx"] = df_itdr.index
 # list of paths of the released datasets
 list_paths_released_ds = glob(path_released_ds)
 list_paths_released_ds.sort()
+
+# if output_mode == 1, set the threshold to 0.99,
+# this should help with floating point comparison 
+if output_mode == 1:
+    threshold_max = 0.999
 
 dict_matches = {}
 num_rows_released = False
@@ -103,11 +109,13 @@ for i_itdr in dict_matches:
     # normalize based on the number of released datasets
     p_dist_row /= float(num_files_released)
 
-    if full_probability:
+    # output_mode == 3, returns full probability
+    if output_mode == 3:
         if len(p_dist_all) == 0:
             p_dist_all = np.vstack([p_dist_row])
         else:
             p_dist_all = np.vstack([p_dist_all, p_dist_row])
+    
     # store indices and values correspond to p_dist_row >= (np.max(p_dist_row)*threshold_max) 
     indx_max_matches = np.where(p_dist_row >= (np.max(p_dist_row)*threshold_max))[0].tolist()
     values_max_matches = p_dist_row[indx_max_matches].tolist()
@@ -116,13 +124,15 @@ for i_itdr in dict_matches:
 # -------- save outputs 
 with open(path_save_max_values, "wb") as output_file:
     pickle.dump(dict_max_matches, output_file)
-if full_probability:
+# output_mode == 3, returns full probability
+if output_mode == 3:
     with open(path_save_p_dist_all, "wb") as output_file:
         pickle.dump(p_dist_all, output_file)
 
 # -------- Plot p.d.f computed in the previous step 
+# This only works with output_mode == 3 (return full probability)
 row_select = 0
-while (row_select >= 0) and full_probability:
+while (row_select >= 0) and (output_mode == 3):
     row_select = int(input("\n\nSelect a row (indexed from 0) in the intruder's dataset. (or enter -1 to exit)  "))
     if row_select < 0:
         break
