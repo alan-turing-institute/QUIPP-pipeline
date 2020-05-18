061859da12704bb853d7502d3bb30581acb928a7
Greg Mingas <gmingas@turing.ac.uk>
2020-05-18 12:22:12 +0100

    Correct errors in correlations.py, make correlation ratios functional, change makefile and edit run inputs

--- utility-metrics/correlations.py
+++ utility-metrics/correlations.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+"""NOTE: Developed and tested with dython=0.5.1"""
+
+import os
 import argparse
 import json
 import numpy as np
-import pandas as pd
-import os
 import pandas as pd
 from dython.nominal import cramers_v, theils_u, correlation_ratio
 
@@ -36,15 +37,11 @@ def cramers_v_corrected_matrix(df):
     output = np.empty([df.shape[1], df.shape[1]])
     array = np.array(df)
 
-    for i1, col1 in enumerate(df.columns):
-        for i2, col2 in enumerate(df.columns):
-            if i1 > i2:
+    for i1 in range(array.shape[1]):
+        for i2 in range(array.shape[1]):
+            if i1 >= i2:
                 output[i1, i2] = cramers_v(array[:, i1], array[:, i2])
-
-    for i1, col1 in enumerate(df.columns):
-        for i2, col2 in enumerate(df.columns):
-            if i1 < i2:
-                output[i1, i2] = output[i2, i1]
+                output[i2, i1] = output[i1, i2]
 
     return output
 
@@ -56,8 +53,8 @@ def theils_u_matrix(df):
     output = np.empty([df.shape[1], df.shape[1]])
     array = np.array(df)
 
-    for i1, col1 in enumerate(df.columns):
-        for i2, col2 in enumerate(df.columns):
+    for i1 in range(array.shape[1]):
+        for i2 in range(array.shape[1]):
             output[i1, i2] = theils_u(array[:, i1], array[:, i2])
 
     return output
@@ -68,11 +65,10 @@ def correlation_ratio_matrix(df, categorical, numeric):
     for all combinations of discrete-continuous columns and fills an output matrix."""
 
     output = np.empty((df[categorical].shape[1], df[numeric].shape[1]))
-    array = np.array(df)
 
     for i1, col1 in enumerate(df[categorical].columns):
         for i2, col2 in enumerate(df[numeric].columns):
-            output[i1, i2] = correlation_ratio(array[:, i1], array[:, i2])
+            output[i1, i2] = correlation_ratio(np.array(df[col1]), np.array(df[col2]))
 
     return output
 
@@ -88,13 +84,13 @@ def utility_measure_correlations(synth_method, path_original_ds, path_original_m
 
     # Divide columns into discrete and numeric,
     # Discrete columns will be later vectorized
-    discrete_features = []
+    categorical_features = []
     numeric_features = []
     for col in orig_metadata['columns']:
         if synth_method == 'sgf':
-            discrete_features.append(col["name"])
-        elif col['type'] in ['Categorical', 'Ordinal', 'DiscreteNumerical', "DateTime"]:
-            discrete_features.append(col["name"])
+            categorical_features.append(col["name"])
+        elif col['type'] in ['Categorical', 'Ordinal', "DateTime"]:
+            categorical_features.append(col["name"])
         else:
             numeric_features.append(col["name"])
 
@@ -103,35 +99,35 @@ def utility_measure_correlations(synth_method, path_original_ds, path_original_m
     orig_df = pd.read_csv(path_original_ds)
     rlsd_df = pd.read_csv(path_released_ds + "/synthetic_data_1.csv")
 
-    # Drop nans
-    #orig_df = orig_df.dropna(axis=0)
-    #rlsd_df = rlsd_df.dropna(axis=0)
-
-    utility_collector = {}
     print("[INFO] Utility measurements")
-    print(30*"-----")
 
     # Calculate Cramer's V and Thiel's U for categorical-categorical combinations of columns
-    cramers_matrix_orig = cramers_v_corrected_matrix(orig_df[discrete_features])
-    cramers_matrix_rlsd = cramers_v_corrected_matrix(rlsd_df[discrete_features])
-    theils_matrix_orig = theils_u_matrix(orig_df[discrete_features])
-    theils_matrix_rlsd = theils_u_matrix(rlsd_df[discrete_features])
+    if len(categorical_features) > 0:
+        cramers_matrix_orig = cramers_v_corrected_matrix(orig_df[categorical_features])
+        cramers_matrix_rlsd = cramers_v_corrected_matrix(rlsd_df[categorical_features])
+        theils_matrix_orig = theils_u_matrix(orig_df[categorical_features])
+        theils_matrix_rlsd = theils_u_matrix(rlsd_df[categorical_features])
 
     # Calculate correlations for continuous-continuous combinations of columns
-    correlation_matrix_orig = np.array(orig_df.corr())
-    correlation_matrix_rlsd = np.array(rlsd_df.corr())
+    if len(numeric_features) > 0:
+        correlation_matrix_orig = np.array(orig_df[numeric_features].corr())
+        correlation_matrix_rlsd = np.array(rlsd_df[numeric_features].corr())
 
     # Calculate correlation ratio for continuous-categorical combinations of columns
-    #correlation_ratio_matrix_orig = correlation_ratio_matrix(orig_df, discrete_features, numeric_features)
-    #correlation_ratio_matrix_rlsd = correlation_ratio_matrix(rlsd_df, discrete_features, numeric_features)
+    if (len(categorical_features) > 0) and (len(numeric_features) > 0):
+        correlation_ratio_matrix_orig = correlation_ratio_matrix(orig_df, categorical_features, numeric_features)
+        correlation_ratio_matrix_rlsd = correlation_ratio_matrix(rlsd_df, categorical_features, numeric_features)
     
     # Store in dictionary after converting numpy arrays to lists in order to allow .json serialisation
-    utility_collector = {"Cramer's V Original": cramers_matrix_orig.tolist(),
-                         "Cramer's V Synthetic": cramers_matrix_rlsd.tolist(),
-                         "Theils's U Original": theils_matrix_orig.tolist(),
-                         "Theils's U Synthetic": theils_matrix_rlsd.tolist(),
-                         "Correlations Original": correlation_matrix_orig.tolist(),
-                         "Correlations Synthetic": correlation_matrix_rlsd.tolist()}
+    utility_collector = {"Cramers_V_Original": cramers_matrix_orig.tolist(),
+                         "Cramers_V_Synthetic": cramers_matrix_rlsd.tolist(),
+                         "Theils_U_Original": theils_matrix_orig.tolist(),
+                         "Theils_U_Synthetic": theils_matrix_rlsd.tolist(),
+                         "Correlations_Original": correlation_matrix_orig.tolist(),
+                         "Correlations_Synthetic": correlation_matrix_rlsd.tolist(),
+                         "Correlation_Ratio_Original": correlation_ratio_matrix_orig.tolist(),
+                         "Correlation_Ratio_Synthetic": correlation_ratio_matrix_rlsd.tolist()
+                         }
 
     with open(output_file_json, "w") as out_fio:
         json.dump(utility_collector, out_fio)
@@ -159,7 +155,7 @@ def main():
     # read parameters from .json
     correlations_parameters = synth_params["parameters_correlations"]
 
-    output_file_json = path_released_ds + "/utility_metric_correlations.json"
+    output_file_json = path_released_ds + "/correlations.json"
 
     np.random.seed(synth_params['parameters']['random_state'])
 
@@ -170,5 +166,6 @@ def main():
                                         output_file_json,
                                         correlations_parameters)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
