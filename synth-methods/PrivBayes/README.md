289bbb89b7e2ac22e2be2cd96de535941c5abefb
Greg Mingas <gmingas@turing.ac.uk>
2020-12-10 19:52:32 +0000

    Add PrivBayes in pipeline, along with an example run-input file

---
+++ synth-methods/PrivBayes/README.md
@@ -0,0 +1,39 @@
+# PrivBayes
+
+**This code uses the PrivBayes implementation within the DataSynthesizer package (https://github.com/DataResponsibly/DataSynthesizer).**
+
+References:
+```tex
+@article{10.1145/3134428,
+author = {Zhang, Jun and Cormode, Graham and Procopiuc, Cecilia M. and Srivastava, Divesh and Xiao, Xiaokui},
+title = {PrivBayes: Private Data Release via Bayesian Networks},
+year = {2017},
+issue_date = {November 2017},
+publisher = {Association for Computing Machinery},
+address = {New York, NY, USA},
+volume = {42},
+number = {4},
+issn = {0362-5915},
+url = {https://doi.org/10.1145/3134428},
+doi = {10.1145/3134428},
+journal = {ACM Trans. Database Syst.},
+month = oct,
+articleno = {25},
+numpages = {41},
+keywords = {Differential privacy, synthetic data generation, bayesian network}
+}
+
+@inproceedings{inproceedings,
+author = {Ping, Haoyue and Stoyanovich, Julia and Howe, Bill},
+year = {2017},
+month = {06},
+pages = {1-5},
+title = {DataSynthesizer: Privacy-Preserving Synthetic Datasets},
+doi = {10.1145/3085504.3091117}
+}
+```
+
+## Additional synthesis parameters
+
+### Example
+
---
+++ synth-methods/PrivBayes/privbayes.py
@@ -0,0 +1,116 @@
+import json
+import os
+import pandas as pd
+import sys
+
+from DataSynthesizer.DataDescriber import DataDescriber
+from DataSynthesizer.DataGenerator import DataGenerator
+from DataSynthesizer.ModelInspector import ModelInspector
+from DataSynthesizer.lib.utils import read_json_file, display_bayesian_network
+
+sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir, "Base"))
+from base import SynthesizerBase
+
+
+class SynthesizerPrivBayes(SynthesizerBase):
+    """Implements dataset synthesis with PrivBayes."""
+
+    def __init__(self):
+        self.metadata = None
+        self.parameters = None
+        self.num_samples_to_synthesize = None
+        self.category_threshold = 10
+        self.epsilon = None
+        self.k = None
+        self.categorical_attributes = None
+        self.datatypes = None
+        self.keys = None
+        self.histogram_bins = None
+        self.random_state = None
+        self.describer = None
+        self.description_file = None
+
+        # instantiate SynthesizerBase from Base directory
+        super().__init__()
+
+    def fit_synthesizer(self, parameters_json_path, csv_path,
+                        metadata_json_path, output_path,
+                        verbose=True):
+        """
+        Wrapper around PrivBayes' DataDescriber. Finds data types and
+        conditional probabilities, creates a description file.
+
+        Parameters
+        ----------
+        parameters_json_path : string
+            Path to a .json file which contains PrivBayes parameters (user inputs)
+        csv_path: string
+            Path to the original dataset
+        metadata_json_path: string
+            Path to the .json file describing the original dataset
+        output_path: string
+            Path where output description file will be written
+        verbose: bool
+            Whether to print all information/warnings output or not
+        """
+
+        with open(metadata_json_path) as metadata_json:
+            self.metadata = json.load(metadata_json)
+
+        # Extract parameters from json parameter file
+        with open(parameters_json_path) as parameters_json:
+            self.parameters = json.load(parameters_json)
+        self.category_threshold = self.parameters['parameters']['category_threshold']
+        self.epsilon = self.parameters['parameters']['epsilon']
+        self.k = self.parameters['parameters']['k']
+        self.keys = self.parameters['parameters']['keys']
+        self.histogram_bins = self.parameters['parameters']['histogram_bins']
+        self.random_state = self.parameters['parameters']['random_state']
+        if verbose:
+            print(f"[INFO] Reading PrivBayes parameters from json file:\n"                  
+                  f"category_threshold = {self.category_threshold}\n"    
+                  f"epsilon = {self.epsilon}\n"   
+                  f"k = {self.k}\n"   
+                  f"random_state = {self.random_state}\n")
+
+        # Convert dataset metadata in .json file to dictionary useable by PrivBayes/DataSynthesizer
+        float_dict = {col['name']: 'Float' for col in self.metadata['columns']
+                      if col['type'] == 'ContinuousNumerical'}
+        int_dict = {col['name']: 'Integer' for col in self.metadata['columns']
+                    if col['type'] == 'DiscreteNumerical'}
+        dt_dict = {col['name']: 'DateTime' for col in self.metadata['columns']
+                   if col['type'] == 'DateTime'}
+        self.datatypes = {**float_dict, **int_dict, **dt_dict}
+        self.categorical_attributes = {col['name']: 'True' for col in self.metadata['columns']
+                                       if col['type'] in ['Categorical', 'Ordinal']}
+
+        # Instantiate describer object, describe the dataset and get probabilities
+        if verbose:
+            print(f"[INFO] Getting variable descriptions and Bayesian Network structure with PrivBayes")
+        self.describer = DataDescriber(category_threshold=self.category_threshold,
+                                       histogram_bins=self.histogram_bins)
+
+        self.describer.describe_dataset_in_correlated_attribute_mode(dataset_file=csv_path,
+                                                                     epsilon=self.epsilon,
+                                                                     k=self.k,
+                                                                     attribute_to_datatype=self.datatypes,
+                                                                     attribute_to_is_categorical=self.categorical_attributes,
+                                                                     attribute_to_is_candidate_key=self.keys,
+                                                                     seed=self.random_state
+                                                                     )
+
+        self.description_file = os.path.join(output_path, "description.json")
+        self.describer.save_dataset_description_to_file(self.description_file)
+        display_bayesian_network(self.describer.bayesian_network)
+
+    def synthesize(self, output_path):
+        """Create synthetic data set using the Bayesian Network."""
+
+        self.num_samples_to_synthesize = self.parameters['parameters']['num_samples_to_synthesize']
+
+        # Synthesize the samples
+        generator = DataGenerator()
+        generator.generate_dataset_in_correlated_attribute_mode(self.num_samples_to_synthesize,
+                                                                self.description_file)
+
+        generator.save_synthetic_data(os.path.join(output_path, "synthetic_data_1.csv"))
