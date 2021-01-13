686037b35ef521aa07dbaa9c5ff929ffa7622c76
kasra-hosseini <khosseini@turing.ac.uk>
2021-01-13 17:03:21 +0000

    Similar to PATE-GAN, if columns_to_synthesize is specified, only synthesize those columns

--- synth-methods/CTGAN/ctgan_main.py
+++ synth-methods/CTGAN/ctgan_main.py
@@ -64,6 +64,22 @@ class SynthesizerCTGAN(SynthesizerBase):
         self.random_state = self.parameters['parameters']['random_state']
         self.num_datasets_to_synthesize = self.parameters["parameters"]["num_datasets_to_synthesize"]
         self.num_samples_to_synthesize = self.parameters["parameters"]["num_samples_to_synthesize"]
+        if "columns_to_synthesize" in self.parameters["parameters"].keys():
+            self.columns_to_synthesize = self.parameters["parameters"]["columns_to_synthesize"]
+        else:
+            self.columns_to_synthesize = "all"
+        
+        if not self.columns_to_synthesize in [None, "all", "None"]:
+            input_data = input_data[self.columns_to_synthesize]
+            metadata_update = {"columns": []}
+            self_metadata_update = {"columns": []}
+            for one_col in metadata["columns"]:
+                if one_col["name"] in self.columns_to_synthesize:
+                    metadata_update["columns"].append(one_col)
+                    self_metadata_update["columns"].append(one_col)
+            metadata = metadata_update
+            self.metadata = self_metadata_update
+
         if verbose:
             print(f"\n[INFO] Reading CTGAN parameters from json file:\n"
                   f"num_samples_to_fit = {self.num_samples_to_fit}\n"
