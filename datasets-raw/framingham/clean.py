9bf9ab747f3f7fdd95ffdf348d75848b8b3e6956
Harrison Wilde <35736580+HarrisonWilde@users.noreply.github.com>
2021-01-29 09:19:36 +0000

    added cleaning code to datasets-raw with raw data

--- datasets-raw/framingham/clean.py
+++ datasets-raw/framingham/clean.py
@@ -5,4 +5,4 @@ raw_df = pd.read_csv("framingham.csv")
 df = raw_df.fillna(raw_df.mean())
 df[["cigsPerDay", "age", "education", "BPMeds"]] = df[["cigsPerDay", "age", "education", "BPMeds"]].astype(int)
 
-df.to_csv("framingham_cleaned.csv", index=False)
+df.to_csv("../../datasets/framinghamframingham_cleaned.csv", index=False)
