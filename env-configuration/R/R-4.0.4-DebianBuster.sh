6df3ab4178c9f4635a03bc03db1b927c4b8a12eb
Oscar Giles <ogiles@turing.ac.uk>
2021-06-24 14:15:58 +0100

    Fix docker-image build

--- env-configuration/R/R-4.0.4-DebianBuster.sh
+++ env-configuration/R/R-4.0.4-DebianBuster.sh
@@ -4,7 +4,11 @@
 apt update -y
 apt install software-properties-common -y
 apt install apt-transport-https -y
-apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
+
+# The following stoped working so adding key as a file
+# apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
+apt-key add env-configuration/R/jranke.asc
+
 echo 'deb http://cloud.r-project.org/bin/linux/debian buster-cran40/' >> /etc/apt/sources.list
 apt update
 apt install -t buster-cran40 r-base r-base-dev -y
